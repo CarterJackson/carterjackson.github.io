b85070bd6eb5a7bee52d6616b479f271be1b28fb
Carter Jackson <carterjackson96@gmail.com>
2023-09-24 16:49:28 -0400

    convert to tabs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [carterjackson.github.io](https://carterjackson.github.io)
+
+TODO: Document GH API limitations
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './views/Header'
 
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-      <div className="flex justify-center">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<h1>Hello World</h1>
+			<div className="flex justify-center">
+				<img src={reactLogo} className="logo react" alt="React logo" />
+			</div>
+		</>
+	)
 }
 
-export default App
+export default App;
