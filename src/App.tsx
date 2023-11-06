10ada6cf7c562f1c96e27bf14222843241755d40
Carter Jackson <carterjackson96@gmail.com>
2023-11-05 21:21:29 -0500

    use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/router';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AppLayout from '@/AppLayout';
+import Projects from '@/components/projects/Projects';
+import About from '@/components/about/About';
 
 function App() {
 	return (
 		<>
-			<RouterProvider router={router} />
+			<Routes>
+				<Route path="/" element={<AppLayout />}>
+					<Route index element={<Navigate to="/about" />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
+				</Route>
+			</Routes>
 		</>
 	)
 }
